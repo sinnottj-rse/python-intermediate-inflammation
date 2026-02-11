72c8b65202d23a38d7e4dae105e4f233512de187
sinnottj-rse <james.r.sinnott@manchester.ac.uk>
2026-02-11 20:03:38 +0000

    Fix pylint issues. Add pylint to build workflow run.

--- inflammation/models.py
+++ inflammation/models.py
@@ -58,12 +58,12 @@ def patient_normalise(data):
     """
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
-    
-    max = np.nanmax(data, axis=1)
-    
+
+    max_infl = np.nanmax(data, axis=1)
+
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_infl[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
-    
+
     return normalised
